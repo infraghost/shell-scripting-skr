cca8515fecf45e88e3d7e833fc364c5bc788d1b9
infraghost <hemasivakishore99@gmail.com>
2025-06-15 18:21:14 +0530

    backend testing pending

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -9,6 +9,10 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 NO="\e[0m"
 
+echo "Please enter DB Password:"
+read -s mysql_root_password
+
+
 VALIDATE() {
     if [ $1 -ne 0 ]; then
         echo -e "$2...$RED Failure $NO"
@@ -33,7 +37,7 @@ VALIDATE $? "Enabling nodejs Version 20"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-id expense
+id expense &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     useradd expense &>>$LOGFILE
@@ -41,3 +45,37 @@ then
 else
     echo "User with name $RED expense $NO already created; so $YELLOW SKIPPING $NO"
 fi
+
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+
+cd /app
+rm -rf /app/* &>>$LOGFILE
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
+
+npm install &>>$LOGGING
+VALIDATE $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Reloading Daemon"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Enabling Backend"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling Backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySql Clinet"
+
+mysql -h db.thehsk.xyz -uroot -p"${mysql_root_password}" < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Loading Schema"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
