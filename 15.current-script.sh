5d135452f058c862ac6680bfaf8b49f79d5d4c38
infraghost <hemasivakishore99@gmail.com>
2025-06-16 12:51:01 +0530

    enhanced

--- 15.current-script.sh
+++ 15.current-script.sh
@@ -2,9 +2,12 @@
 
 COURSE="DevOps From Current Script"
 
-echo "Before calling other script, Course: $COURSE"
+echo "Before calling Current script, Course: $COURSE"
 echo "Process ID of current script: $$"
 
 ./16.Other-Script.sh
+
+echo "With Source Command"
+source ./16.Other-Script.sh
 
 echo "After calling other script, Course: $COURSE"
--- 16.Other-Script.sh
+++ 16.Other-Script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Other Script"
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NO="\e[0m"
+
+COURSE="Cloud From Current Script"
+
+echo "$GREEN Before calling other script, Course:$NO $COURSE"
+echo "$GREEN Process ID of Other script:$NO $$"
+
+echo "$RED 16-Other Script $NO"
