28538d47e60c94568292c65c361a3faf7c4bbcd4
infraghost <hemasivakishore99@gmail.com>
2025-06-19 19:49:59 +0530

    trap command testing

---
+++ 18.trap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+handle_error(){
+    echo "Error Occured at line number: $1, error command is $2"
+}
+
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+apt install gittttt -t
